326f8ee8dc04d5d7f11976935c2138cd26c8e04f
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-26 20:36:56 -0300

    Adicionando layout com Bootstrap

---
+++ Loja/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#table-produtos').DataTable({
+        "language": {
+            search: '<i class="fa fa-filter" aria-hidden="true"></i>',
+            searchPlaceholder: 'Buscar produto'
+        }
+});
+    autoFill: true;
+   
+});
+
+
+
